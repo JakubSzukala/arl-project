3d523096756fa94d2c58f94f14bf1152c9307238
JakubSzukala <burski09@outlook.com>
2022-12-13 08:21:35 +0100

    Add demo stub with /CS GPIO initialization

---
+++ examples/app_spi_demo/src/hello_world.c
@@ -0,0 +1,83 @@
+/**
+ * ,---------,       ____  _ __
+ * |  ,-^-,  |      / __ )(_) /_______________ _____  ___
+ * | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * | / ,--´  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *    +------`   /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * Crazyflie control firmware
+ *
+ * Copyright (C) 2019 Bitcraze AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * hello_world.c - App layer application of a simple hello world debug print every
+ *   2 seconds.
+ */
+
+
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "app.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define DEBUG_MODULE "HELLOWORLD"
+#include "debug.h"
+
+/**
+ * Remarks
+ * - Double check the SPI mode of operation
+ * - SPI1 (PA(5-7)) is exposed on the header
+ * - This means, we can use the driver from src/deck/api/deck_spi.c it is hard coded for SPI1
+ * - It will be the same api that we will use for decks (duh...)
+ * - It is a little bit cringe, that it is called as 'deck' but is actually pretty generic
+ * - If CSB is pulled down, the SPI interface is activated (BMP280)
+ * - Write a function that will write a byte and then verify this write with a read
+ * - But also add some temperature read for good measure
+ * - CTRL_MEAS OSRS_T registers for temperature oversampling should be good
+ *     Temperature meas skip - Pressure mesure skip - Normal mode
+ *   - 000 000 11
+ *   - Then output when reading the values should be fixed to 0x80000
+ * - Maybe sequence could be:
+ *   - After power on reset read the chip id should be 0x58
+ *   - Set the normal mode (default after power on reset is sleep), with validation
+ *   - Read temperature in loop to se if it changes
+ * - 
+ **/
+
+//static void register_write()
+
+void appMain() {
+  DEBUG_PRINT("Waiting for activation ...\n");
+
+  // Initialize GPIO pin as SPI /CS for BMP280
+  GPIO_InitTypeDef gpio_init_struct = {
+    .GPIO_Pin = GPIO_Pin_4,
+    .GPIO_Mode = GPIO_Mode_OUT,
+    .GPIO_Speed = GPIO_Low_Speed,
+    .GPIO_OType = GPIO_OType_PP,
+    .GPIO_PuPd = GPIO_PuPd_NOPULL
+  };
+  GPIO_Init(GPIOB, &gpio_init_struct);
+  GPIO_WriteBit(GPIOB, GPIO_Pin_4, 1); // Disable on init
+
+  while(1) {
+    vTaskDelay(M2T(2000));
+    DEBUG_PRINT("Hello World!\n");
+  }
+}
